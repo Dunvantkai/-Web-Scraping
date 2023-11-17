dad236d968d43e4feb3b47a62988447055f49fef
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2023-11-17 20:13:15 +1300

    Update Html WebScraper.py

--- Html WebScraper.py
+++ Html WebScraper.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 website = input("Please Enter the websites url or type eg for the exsaple website: ")
 if website == "eg":
-    website = ("https://subslikescript.com/movie/Shrek-126029")
+    website = ("https://warthunder.com/en")
 result = requests.get(website)
 content = result.text
-txtfile = open("Website.txt", "w") 
-htmlfile = open("Website.html", "w") 
+print(content)
+EXIT = input("Done. Press Enter to Exit the Program")
+txtfile = open("Website.txt", "w", encoding="utf-8") 
 txtfile.write(content)
+htmlfile = open("Website.html", "w", encoding="utf-8") 
 htmlfile.write(content)
 EXIT = input("Done. Press Enter to Exit the Program")
